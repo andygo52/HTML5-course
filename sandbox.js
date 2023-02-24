258afa038b70b0f4facd333860fdfc37b043b577
Andy Goldstein <andy.goldstein@palmbeachschools.org>
2023-02-24 16:44:00 -0500

    Lesson 9 - Forms - Partial

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,10 @@
 console.log('Hello from sandbox.js');
 
 console.log(2 + 2);
+
+//This is a comment.
+
+//When it comes to JavaScript, I have not really done it since last summer and I feel rusty.
+//I am working on doing the Dave Gray courses on HTML5, then CSS3, then JavaScript.
+//I love the way he teaches in his HTML5 course, and I am learning a lot.
+//I can't wait to integrate it with CSS3!
